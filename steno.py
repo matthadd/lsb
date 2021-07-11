f8d991a9108e2bc9ce1b3e980c59bc00ea7f6687
matthadd <74564648+matthadd@users.noreply.github.com>
2021-07-11 19:54:07 +0200

    test
    test

--- steno.py
+++ steno.py
@@ -1,39 +1,31 @@
 import numpy as np
 from PIL import Image
-from termcolor import colored
 
 
-def decode_image():
-    file_data = []
-    for i in range(len(pixels)):
-        # file_data.append(pixels[i])
-        # print(file_data)
-        for j in range(len(pixels[0])):
-            for k in range(len(pixels[0][0])):
-                # print(pixels[i][j][k])
-                file_data.append(format(pixels[i][j][k], "b"))
-                # print("--- %s seconds ---" % (time.time() - start_time))
-                # print(file_data)
-    print("file_data complete")
-
-
-def inverse_decode_image():
-    pass
-
-
-def encode(source, message ):
-    source = 'IMG_7834.jpeg'
+def encode(source, message, destination):
+    source = 'images.jpg'
     message = 'test'
+    destination = 'out.jpeg'
 
     image = Image.open(source, 'r')
     width, height = image.size
     print('[+] lsb start')
-    pixels = np.array(list(image.getdata()))
-    print(pixels)
+    data = np.array(list(image.getdata()))
     message += 'stenoEOF'
+
+    print('')
+    print(data)
+    print(width * height, width, height)
+
+
+    img = Image.fromarray(data, 'RGB')
+    img.save(destination)
+    img.show()
+
+    print('[+] lsb end')
 
 
 source = ""
 message = ""
-encode(source, message)
-
+destination = ""
+encode(source, message, destination)
