6ec8d1e47d9b5ab6be6f008c71b646ae4ca4e9f0
matthadd <74564648+matthadd@users.noreply.github.com>
2021-08-24 14:48:30 +0200

    final version

--- steno.py
+++ steno.py
@@ -14,14 +14,11 @@ class Hide():
         self.message_b = open(self.file_name, 'rb').read()
 
     def encode(self):
-        print(self.message_b)
         image = Image.open(self.image_source)
-        print('[+] lsb start write image')
         data = np.array(image)
         self.message_b = ''.join([format(i, "08b") for i in self.message_b])
         # header = format(len(self.message_b), "08b")
         header = '{:032b}'.format(len(self.message_b))
-        print('input', header, ".", self.message_b)
         self.message_b = header + self.message_b
 
         def hide(data):
@@ -29,26 +26,17 @@ class Hide():
             for i in range(len(data)):
                 for j in range(len(data[0])):
                     for k in range(len(data[0, 0])):
-                        # print(data[i,j,k], end = " ")
                         var = format(data[i, j, k], "08b")
-                        # print(int(var, 2), var, end=" ")
                         var = var[:-1] + str(self.message_b[index])
-                        # print(self.message_b[index], var, int(var, 2), end=" ")
                         index += 1
                         var = int(var, 2)
                         data[i, j, k] = var
-                        # print(data[i, j, k])
                         if index == len(self.message_b):
                             return None
 
-        print(data)
         hide(data)
-        data[0, 0, 0] = 0
-        print(data)
         im = Image.fromarray(data)
-        im.save(image_output)
-        # im.show()
-        print('[+] Finish writing data to image')
+        im.save(self.image_output)
 
 
 class Dehide():
@@ -59,15 +47,12 @@ class Dehide():
     def dehide(self):
         image = Image.open(self.image_output)
         index = 0
-        print('[+] lsb start reading image')
         data = np.array(image)
-        print(data)
         for i in range(len(data)):
             for j in range(len(data[0])):
                 for k in range(len(data[0, 0])):
                     index += 1
                     number = data[i, j, k]
-                    print(number, format(number, "08b"), format(number, "08b")[-1])
                     number = format(number, "08b")
                     self.message_b += number[-1]
                     if index > 2 * HEADER_SIZE:
@@ -75,20 +60,43 @@ class Dehide():
 
     def result(self):
         message_lenght = int(self.message_b[:HEADER_SIZE], 2)
-        print("final", self.message_b[:HEADER_SIZE], ".", self.message_b[HEADER_SIZE:message_lenght + HEADER_SIZE], ".",
-              self.message_b[message_lenght + HEADER_SIZE:])
+        header = self.message_b[:HEADER_SIZE]
+        body = self.message_b[HEADER_SIZE:message_lenght + HEADER_SIZE]
+        return body
 
-    print('[+] lsb end')
-
-
-# Parameters from user
-file_name = 'test.txt'
-image_source = 'hs.jpeg'
-image_output = 'hsout.jpeg'
 
 # main
-hide = Hide(file_name, image_source, image_output)
-hide.encode()
-dehide = Dehide(image_output)
-dehide.dehide()
-dehide.result()
+def main():
+    # Parameters from user
+    file_name = 'test.txt'
+    image_source = 'test.png'
+    image_output = 'out.png'
+    print("[+] LSB STENOGRAPHY TOOL OF A VERY HIGH STANDING")
+    print("1 - Write hidden data to image ")
+    print("2 - Read hidden data from image")
+    choice = int(input("Enter your option : "))
+    if choice == 1:
+        print('[+] Hiding data ...')
+        hide = Hide(file_name, image_source, image_output)
+        hide.encode()
+        print('[+] Finish writing data to image')
+    elif choice == 2:
+        print('[+] Reading data ...')
+        dehide = Dehide(image_output)
+        dehide.dehide()
+        data = dehide.result()
+        print('[+] Finish reading data from image')
+
+        def bitstring_to_bytes(s):
+            v = int(s, 2)
+            b = bytearray()
+            while v:
+                b.append(v & 0xff)
+                v >>= 8
+            return bytes(b[::-1])
+        data = bitstring_to_bytes(data)
+        open("out.txt", "wb").write(data)
+        print('[+] Finish writing data to file')
+        print(open("out.txt", "r").read())
+
+main()
